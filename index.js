ce6f4b76a0ee9ffa347903fe1375b79f847902b7
VenumiV <venumivonara@gmail.com>
2024-07-22 21:21:16 +0530

    final

--- index.js
+++ index.js
@@ -12,24 +12,6 @@
         }
       }
        
-    /*  function viewMore(menuId){
-        
-        
-        if(menuId='menu1'){
-          document.getElementById('menu1').innerHTML = "The perfect balance of espresso, steamed milk and foam.";
-        }else 
-        if(menuId = 'menu2'){
-          document.getElementById('menu2').innerHTML = " A concentrated form of coffee served in small, strong shots";
-        }
-        else if(menuId = 'menu3'){
-          document.getElementById('menu3').innerHTML =  "Made with caramel syrup, espresso shots, milk, and ice ";
-        }else if(menuId ='menu4'){
-          document.getElementById('menu4').innerHTML = "Made with dark, semisweet, or bittersweet chocolate chopped into small pieces stirred into milk with the addition of sugar.";
-        }
-        
-        
-       
-      }*/
 
         function viewMore(menuId) {
           const description = document.getElementById(menuId + '-desc');
